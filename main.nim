38ce186cf3881da781c221dac91c45d370010afd
Rosen Popov <rpopov@qti.qualcomm.com>
2020-11-17 15:41:53 +0200

    Added primitive data structure

--- main.nim
+++ main.nim
@@ -1,8 +1,32 @@
 #import xmltree
 import httpClient
+import htmlparser
+import xmltree
+#import strtabs
 
-var tmp_clinet = newHttpClient()
+type
+  Property = object 
+    xTag:string
+    prop:string
+    value:string
 
-echo tmp_clinet.getContent("https://google.com")
+  Group = object
+    collums:seq[Property]
 
+  TagSpec = object
+    targets:seq[Group]
 
+  SpiderDen = object
+    rootpoint:string
+    page_que:seq[string]
+    next_page:Property
+
+proc WeaveWeb(spec:var SpiderDen)=
+  var tmp_clinet = newHttpClient()
+  var page_seq_swp : seq[string]
+  while spec.page_que.len() != 0:
+    for crnt_page in spec.page_que:
+      echo crnt_page
+
+if isMainModule == true:
+  echo "is not done"
