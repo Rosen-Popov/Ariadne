bfd3abdb765a2f4e30e59cfc9ab4c0bec866b9d6
Rosen Popov <rpopov@qti.qualcomm.com>
2020-11-26 14:53:23 +0200

    Added a more complex criteria to make matching more flexible

--- scraper.nim
+++ scraper.nim
@@ -1,15 +1,22 @@
-import httpClient
+import re
 import htmlparser
 import xmltree
 import sets
 import tables
 import bitops
+import strtabs
 import strformat
+import httpClient
+import locks
 
-const GetText:int = 1
-const PrepRoot:int = 2
-const UseText:int = 4
-const UseUid:int = 8
+const GetText:int =       1 shl 0
+const PrepRoot:int=       1 shl 1
+const UseText:int =       1 shl 2
+const UseUid:int =        1 shl 3
+const TextIsRegex:int=    1 shl 4
+const UidIsRegex:int =    1 shl 5
+const UidVIsRegex:int =   1 shl 6
+
 
 type
   TagId = object
@@ -29,6 +36,13 @@ type
     FailNonEqu:bool
     PageLimit:int
 
+  ThrSafeGlRegexTable =object
+    #@TODO make functions to deal with that
+    mutex:Lock
+    table:Table[string,Regex]
+
+var PrcRg:ThrSafeGlRegexTable
+
 proc ComplId(tar:TagId):string=
   return tar.Uid & "=" & tar.UidV
 
@@ -41,25 +55,46 @@ proc `&`(a,b:int):int=
 proc `?`(mask,item:int):bool=
   return bitand(mask,item).bool
 
-#proc getnext_pages(dom:XmlNode,target_tag:TagId,root:string):HashSet[string]=
-#  var res : HashSet[string]
-#  #@TODO : Make it so it's a switchcase or more flexible
-#  for i in dom.findAll(target_tag.XTag):
-#    if i.innerText == target_tag.Text:
-#      res.incl(root & i.attr(target_tag.Prop))
-#  return res
-proc getnext_pages(dom:XmlNode,target_tag:TagId,root:string):HashSet[string]=
-  var res : HashSet[string]
+#@TODO :Make documentation
+proc Elegiable(test_tag:XmlNode,ctlTag:TagId):bool=
   var Filter:bool = true
-  #@TODO : Make it so it's a switchcase or more flexible
+  if ctlTag.Flags ? UseText:
+    if ctlTag.Flags ? TextIsRegex:
+      Filter = Filter and
+      match(test_tag.innerText,PrcRg.table[ctlTag.Text])
+    else:
+      Filter = Filter and test_tag.innerText == ctlTag.Text
+  block UidUidVMatchPair:
+    if (ctlTag.Flags ? UseUid) and Filter == true:
+      case ctlTag.Flags & (UidVIsRegex | UidIsRegex):
+        of UidVIsRegex | UidIsRegex:
+          for prop in test_tag.attrs().keys():
+            if match(prop,PrcRg.table[ctlTag.Uid]) and match(test_tag.attr(prop), PrcRg.table[ctlTag.UidV]):
+              Filter = true
+              break UidUidVMatchPair
+          Filter = false
+        of UidIsRegex:
+          for prop in test_tag.attrs().keys():
+            if match(prop,PrcRg.table[ctlTag.Uid]) and test_tag.attr(prop) == ctlTag.UidV:
+              Filter = true
+              break UidUidVMatchPair
+          Filter = false
+        of UidVIsRegex:
+          Filter = Filter and match(test_tag.attr(ctlTag.Uid),PrcRg.table[ctlTag.UidV])
+        else :
+          Filter = Filter and test_tag.attr(ctlTag.Uid) == ctlTag.UidV
+  return Filter
+
+#@TODO :Make documentation
+proc getnext_pages(dom:XmlNode,target_tag:TagId,root:string):HashSet[string]=
+  # @TODO make it os it uses the same hashset
+  var res : HashSet[string]
   for i in dom.findAll(target_tag.XTag):
-    if target_tag.Flags ? UseText:
-      Filter
-    if i.innerText == target_tag.Text:
+    if i.Elegiable(target_tag):
       res.incl(root & i.attr(target_tag.Prop))
   return res
 
-
+#@TODO :Make documentation
 proc SmartPrep(Uri,text:string):string=
   if text[0] == '/' and Uri[Uri.high()] == '/':
     return Uri[0..<Uri.high()] & text
@@ -68,6 +103,12 @@ proc SmartPrep(Uri,text:string):string=
   elif text[0] != '/' and Uri[Uri.high()] == '/':
     return Uri & text
 
+#@TODO :Make documentation
+proc PrecompileRegexes(nextpage:TagId,targets:seq[TagId])=
+  #@TODO precompile regexes
+  return
+
+#@TODO :Make documentation
 proc WeaveWeb*(spec:var SpiderDen):Table[string,seq[string]]=
   var tmp_client = newHttpClient()
   var newPages : HashSet[string]
@@ -76,6 +117,7 @@ proc WeaveWeb*(spec:var SpiderDen):Table[string,seq[string]]=
   var sanity_check:int = -1
   var sanity_key: string
   var page:int = 0
+  PrecompileRegexes(spec.NextPage,spec.Targets)
 
   for i in spec.Targets:
     Storage[i.ComplId()] = @[]
@@ -90,6 +132,7 @@ proc WeaveWeb*(spec:var SpiderDen):Table[string,seq[string]]=
         newPages.incl(getnext_pages(dom,spec.NextPage,spec.Rootpoint))
         for targets in spec.Targets:
           for selection in dom.findAll(targets.Xtag):
+            #@TODO make this use Elegiable
             if selection.attr(targets.Uid) == targets.UidV and selection.attr(targets.Prop) != "":
               if targets.Flags ? PrepRoot:
                 Storage[targets.ComplId() ].add(SmartPrep(spec.RootPoint,selection.attr(targets.Prop)))
